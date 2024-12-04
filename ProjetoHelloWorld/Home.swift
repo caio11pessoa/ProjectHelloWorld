728ab9eb792eb3544b6bbccfa5a9e0ac87ab6086
CaioPessoa <caiopessoa1298@gmail.com>
2024-12-04 18:28:50 -0300

    Initial Commit

---
+++ ProjetoHelloWorld/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  ProjetoHelloWorld
+//
+//  Created by Caio de Almeida Pessoa on 04/12/24.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text("Home")
+    }
+}
+
+#Preview {
+    Home()
+}
---
+++ ProjetoHelloWorld/ProjetoHelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetoHelloWorldApp.swift
+//  ProjetoHelloWorld
+//
+//  Created by Caio de Almeida Pessoa on 04/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ProjetoHelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Home()
+        }
+    }
+}
